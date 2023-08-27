7cd7bda954f3f860a4e639c6d97c4b8303ec3d06
choiwonseokgit <cs0328@hanmail.net>
2023-08-27 21:27:49 +0900

    두번째 commit

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const config = require("./config/key");
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+//application/x-www-form-urlencoded 의 형태의 데이터를 서버에서 분석해서 가져올수 있게 해줌 이거 있어야 데이터 구분 가능
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 의 형태의 데이터를 서버에서 분석해서 가져올수 있게 해줌 이거 있어야 데이터 구분 가능
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,4 +25,18 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -25,6 +27,31 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre("save", function (next) {
+  //registerRoute의 save로 넘어가기 전에 실행되는 함수이다. userSchema.pre()
+  let user = this;
+
+  //user의 password가 변환될때만 실행(생성도 변환이기 때문에 처음할때 함수 실행됨)
+  if (user.isModified("password")) {
+    //bcrypt.genSalt를 이용하여 salt를 생성
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      //위의 salt로 비밀번호를 hash(암호화)한다.
+      bcrypt.hash(
+        user.password,
+        salt /* 위의 cb함수에서 내려온 salt */,
+        function (err, hash) {
+          if (err) return next(err);
+
+          user.password = hash; //user.password 를 hash 로 교체
+          next();
+        }
+      );
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
